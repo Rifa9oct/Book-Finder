28d9a88b3a5722e0312861a062f044842734c3b9
Rifa9oct <kohinuraktherr@gmail.com>
2024-01-16 16:05:38 +0600

    sho

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,13 +1,19 @@
+import Books from "../components/Books";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
 const Root = () => {
-    return (
-        <div>
-           <Navbar></Navbar>
-           <Header></Header>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="my-10 lg:my-14">
+        <Header></Header>
+        <Books></Books>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
---
+++ src/components/Books.jsx
@@ -0,0 +1,89 @@
+import bookImg from "./../assets/book.png"
+import BookCard from "./BookCard";
+
+const Books = () => {
+    const booksItem = [
+        {
+            'id': 1,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2023',
+            'isFavorite': false,
+        },
+        {
+            'id': 2,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2020',
+            'isFavorite': false,
+        },
+        {
+            'id': 3,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2018',
+            'isFavorite': false,
+        },
+        {
+            'id': 4,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2016',
+            'isFavorite': false,
+        },
+        {
+            'id': 5,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2022',
+            'isFavorite': false,
+        },
+        {
+            'id': 6,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2010',
+            'isFavorite': false,
+        },
+        {
+            'id': 7,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2012',
+            'isFavorite': false,
+        },
+        {
+            'id': 8,
+            'name': 'JavaScript and Jquery',
+            'image': `${bookImg}`,
+            'writer': 'Jon Duckett',
+            'price': '$62',
+            'year': '2014',
+            'isFavorite': false,
+        },
+    ]
+
+    return (
+        <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {
+              booksItem.map(book => <BookCard key={book.id} book={book}></BookCard>)  
+            }
+        </div>
+    );
+};
+
+export default Books;
