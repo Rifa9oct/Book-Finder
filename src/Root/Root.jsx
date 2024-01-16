a5d4d7125f1a29a384854b5741512af327b37778
Rifa9oct <kohinuraktherr@gmail.com>
2024-01-16 15:22:29 +0600

    added header section

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,11 @@
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 
 const Root = () => {
     return (
         <div>
            <Navbar></Navbar>
+           <Header></Header>
         </div>
     );
 };
